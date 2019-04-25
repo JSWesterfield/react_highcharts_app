686412fe86e46f86d1374976c597d38bd73a5893
Jacob Westerfield <jwesterfield@sosacorp.com>
2019-04-25 18:58:28 -0400

    getting compilation error taking a break for a sec but I think it has something to do with a line in the code. Will debug next

--- hcreact/src/index.js
+++ hcreact/src/index.js
@@ -1,33 +1,27 @@
-//import React from 'react';
-//import ReactDOM from 'react-dom';
-//import './index.css';
-//import App from './App';
-//import * as serviceWorker from './serviceWorker';
-
-//ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
-
 import React from 'react';
 import { render } from 'react-dom';
-import Highcharts from 'highcharts';
+import Highcharts from 'highcharts/highstock';
 import HighchartsReact from 'highcharts-react-official';
 
 const options = {
-    chart: {
-        type: 'spline'
-    },
-    title: {
-        text: 'My Chart'
-    },
-    series: [
-        {
-            data: [1, 2, 1, 4, 3, 6]
-        }
-    ]
-        
-    
-}
+  title: {
+    text: 'My stock chart'
+  },
+  series: [
+    {
+      data: [1, 2, 1, 4, 3, 6, 7, 3, 8, 6, 9]
+    }
+  ]
+};
+
+const App = () => (
+  <div>
+    <HighchartsReact
+      highcharts={Highcharts}
+      constructorType={'stockChart'}
+      options={options}
+    />
+  </div>
+);
+
+render(<App />, document.getElementById('root'));
